19825cb553ebfc5e34d25ae5233709a2b6358a8a
Spencer Penrod <spencerpenrod@gmail.com>
2023-01-03 16:50:41 -0700

    Feat: add function to add books to a library array

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, numPages, haveRead) {
   this.title = title;
   this.author = author;
@@ -9,3 +11,9 @@ Book.prototype.info = function info() {
   const isItRead = this.haveRead ? 'already read' : 'not read yet';
   return `${this.title} by ${this.author}, ${this.numPages} pages, ${isItRead}.`;
 };
+
+function addBookToLibrary(book) {
+  if (!(book instanceof Book)) throw new TypeError("That's no book!");
+
+  myLibrary.push(book);
+}
