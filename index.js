6fa147ee5336a78c8957feac4851f24a82b3f5af
Spencer Penrod <spencerpenrod@gmail.com>
2023-01-03 20:12:15 -0700

    Feat: add function to display library onto page

--- index.js
+++ index.js
@@ -17,3 +17,42 @@ function addBookToLibrary(book) {
 
   myLibrary.push(book);
 }
+
+function addBooksToPage() {
+  const library = document.querySelector('.library');
+
+  myLibrary.forEach((book) => {
+    const newBook = document.createElement('div');
+    newBook.classList.add('book');
+
+    const title = document.createElement('h3');
+    title.classList.add('title');
+    const titleText = document.createTextNode(book.title);
+    title.appendChild(titleText);
+
+    const author = document.createElement('p');
+    author.classList.add('author');
+    const authorText = document.createTextNode(book.author);
+    author.appendChild(authorText);
+
+    const pages = document.createElement('small');
+    pages.classList.add('pages');
+    const pagesText = document.createTextNode(book.numPages);
+    pages.appendChild(pagesText);
+
+    const readStatus = document.createElement('p');
+    readStatus.classList.add('readStatus');
+    const isItRead = book.haveRead ? 'Aleady read' : 'Not read yet';
+    const status = book.haveRead ? 'read' : 'unread';
+    readStatus.classList.add(status);
+    const readText = document.createTextNode(isItRead);
+    readStatus.appendChild(readText);
+
+    newBook.appendChild(title);
+    newBook.appendChild(author);
+    newBook.appendChild(pages);
+    newBook.appendChild(readStatus);
+
+    library.appendChild(newBook);
+  });
+}
